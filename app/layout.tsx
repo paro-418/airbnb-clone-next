d755ff711d116dfc342d72881472630acc56de0c
Praveen kumar verma <vermapraween6543@gmail.com>
2023-04-08 14:24:20 +0530

    modal completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,33 @@
-import './globals.css'
+'use client';
+import './globals.css';
+import { Nunito } from 'next/font/google';
+import NavBar from './components/navbar/NavBar';
+import ClientOnly from './components/ClientOnly';
+import Modal from './components/Modals/Modal';
+
+const font = Nunito({
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <Modal isOpen onSubmit={() => {}} onClose={() => {}} actionLabel='' />
+          <NavBar />0
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
